66615d34b11684e7927951f9e68c98fa1294992f
bhavanapatil2901 <bhavanapatil2901@gmail.com>
2025-07-17 19:51:15 +0530

    save

---
+++ Student.java
@@ -0,0 +1,11 @@
+ class Student {
+    public static void main(String[] args)
+    {
+        System.out.println("student details");
+        System.out.println("Name:bhavana");
+        System.out.println("depatment:bca");
+        System.out.println("subject:devops");
+        System.out.println("addrass:raichur");
+
+    }
+}
