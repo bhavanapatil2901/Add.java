8ebddb9ba72bb1725f5756e4f8281f1a90a82674
bhavanapatil2901 <bhavanapatil2901@gmail.com>
2025-07-17 10:06:25 +0530

    save

---
+++ employe.java
@@ -0,0 +1,11 @@
+class employe {
+    public static void main(String[] args)
+    {
+        System.out.println("employe details");
+        System.out.println("Name:bhavana");
+        System.out.println("id:12345");
+        System.out.println("salary:25000");
+        System.out.println("addrass:raichur");
+
+}
+}
