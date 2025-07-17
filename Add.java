9391c0fdf1f881fd19180d444cdd4d1e9d27a889
bhavanapatil2901 <bhavanapatil2901@gmail.com>
2025-07-17 09:50:39 +0530

    save

---
+++ Add.java
@@ -0,0 +1,11 @@
+class Add {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("value of a"+a);
+        System.out.println("value of b"+b);
+        System.out.println("Addition of a&b is"+c);
+    }
+}
