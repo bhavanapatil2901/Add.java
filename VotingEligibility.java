5708f68d7ccba12c3fd1e69f780deae8c277b29a
ARSHADMULLAA <arshadmulla874@gmail.com>
2025-08-31 17:33:28 +0530

    save

---
+++ VotingEligibility.java
@@ -0,0 +1,14 @@
+public class VotingEligibility {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("User is eligible to vote.");
+        } else {
+            System.out.println("User is not eligible to vote.");
+        }
+    }
+}
+
+ 
+    
+
